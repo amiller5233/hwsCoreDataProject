f3f100875a3aaa4e888b931b66052b3d7449d0f6
amiller5233@gmail.com <amiller5233@gmail.com>
2023-08-13 13:04:11 -0700

    Manually create a NSManagedObject subclass

---
+++ CoreDataProject/Movie+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Movie+CoreDataClass.swift
+//  CoreDataProject
+//
+//  Created by Adam Miller on 8/13/23.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Movie)
+public class Movie: NSManagedObject {
+
+}
