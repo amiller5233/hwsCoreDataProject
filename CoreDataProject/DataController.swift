ab838540c212cd23693a269b79f82943fd880db6
amiller5233@gmail.com <amiller5233@gmail.com>
2023-08-13 12:36:15 -0700

    create xcode project and copy DataController

---
+++ CoreDataProject/DataController.swift
@@ -0,0 +1,21 @@
+//
+//  DataController.swift
+//  CoreDataProject
+//
+//  Created by Adam Miller on 8/13/23.
+//
+
+import CoreData
+import Foundation
+
+class DataController: ObservableObject {
+    let container = NSPersistentContainer(name: "CoreDataProject")
+    
+    init() {
+        container.loadPersistentStores { description, error in
+            if let error = error {
+                print("CoreData failed to load: \(error.localizedDescription)")
+            }
+        }
+    }
+}
