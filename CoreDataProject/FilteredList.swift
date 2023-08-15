4dab691c91e10fc20c22574cf1c1876067ea81eb
amiller5233@gmail.com <amiller5233@gmail.com>
2023-08-14 18:36:10 -0700

    filtering FetchRequest

---
+++ CoreDataProject/FilteredList.swift
@@ -0,0 +1,22 @@
+//
+//  FilteredList.swift
+//  CoreDataProject
+//
+//  Created by Adam Miller on 8/14/23.
+//
+
+import SwiftUI
+
+struct FilteredList: View {
+    @FetchRequest var fetchRequest: FetchedResults<Singer>
+    
+    var body: some View {
+        List(fetchRequest, id: \.self) { singer in
+            Text("\(singer.wrappedFirstName) \(singer.wrappedLastName)")
+        }
+    }
+    
+    init(filter: String) {
+        _fetchRequest = FetchRequest<Singer>(sortDescriptors: [], predicate: NSPredicate(format: "lastName BEGINSWITH %@", filter))
+    }
+}
---
+++ Singer+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Singer+CoreDataClass.swift
+//  CoreDataProject
+//
+//  Created by Adam Miller on 8/14/23.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Singer)
+public class Singer: NSManagedObject {
+
+}
